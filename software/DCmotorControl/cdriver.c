1867984acf1c8a796794034159561722370f5696
Kareem Mohamed <kareem.aboelmkarem@gmail.com>
2019-04-18 00:16:24 +0200

    the comments in the cdriver.c file is updated to be more readable

--- software/DCmotorControl/cdriver.c
+++ software/DCmotorControl/cdriver.c
@@ -2,7 +2,7 @@
  ============================================================================
  Name        : cdrivers.c
  Author      : kareem mohamed
- Description : define the used data types  in embedded programming
+ Description :in this file we define the modules functions of the external interrupt0 and pwm mode for timer 0 in avr micro controller
  Date        : 14/4/2019
  ============================================================================
  */
@@ -18,20 +18,20 @@
 // first function to initialize the fast PWM mode in atmega16 to control the speed of DC motor
 void Init_pwm_timer0(void)
 {
-	TCCR0 =(1<<WGM00)|(1<<WGM01)|(1<<COM01)|(1<<CS01);
 	/*
 	 * WGM00 AND WGM01 ARE SET TO CHOOSE FAST PWM MODE
 	 * COM01 IS SET AND COM00 IS CLEARED TO CHOOSE NON-INVERTING MODE
 	 * CS01 IS SET AND CS00 AND CS02 ARE CLEARED TO CHOOSE PRESCALER =8
 	 * THE PWM FREQUENCY IS 488 HZ = (1MHZ/(256*8))
 	 */
-	TCNT0 =0;
+	TCCR0 =(1<<WGM00)|(1<<WGM01)|(1<<COM01)|(1<<CS01);
 	// Initial value for TCNT0 register =0 to start count from 0;
-	OCR0=0;
+	TCNT0 =0;
 	// Initial value for OCR0 register =0,THIS REGISTER WILL BE SET BY THE USER TO CONTROL SPEED OF DC MOTOR
+	OCR0=0;
+    // PIN (PB3) OR (OC0) IS SET TO BE OUTPUT PIN TO CONTROL THE SPEED OF DC MOTOR
 	DDRB_REG.BITS.BIT3 = HIGH;
-	// PIN (PB3) OR (OC0) IS SET TO BE OUTPUT PIN TO CONTROL THE SPEED OF DC MOTOR
-
+	
 }
 
 /* second function to initialize the external interrupt0 to allow the user to change the speed of dc motor
@@ -42,14 +42,15 @@ void Init_pwm_timer0(void)
  */
 void Init_external_interrupt0(void)
 {
-	SREG |=(1<<7);
 	// TO ENABLE GLOBAL INTERRUPT FLAG (I-BIT)
-	MCUCR |=(1<<ISC00)|(1<<ISC01);
+	SREG |=(1<<7);
 	// THE ISC00 AND ISC01  BITS AER SET TO 1 TO GENERATE THE INTERRUPT AT RISING EDGE.
-	GICR |=(1<<INT0);
+	MCUCR |=(1<<ISC00)|(1<<ISC01);
 	// IS SET TO ENABLE INTERRUPT FLAG OF EXTERNAL INTERRUPT0.
-	DDRD_REG.BITS.BIT2=LOW;
+	GICR |=(1<<INT0);
 	// CLearing pin PB0 To make it input form external interrupt 0 to control the speed of DC MOTOR
+	DDRD_REG.BITS.BIT2=LOW;
+	
 
 }
 
